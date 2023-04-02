262b48d61cddcff74cb9d34538e1cf3e2fca5c1b
Dev1nxavier <seangreenebrandeis@gmail.com>
2023-04-02 13:21:10 -0400

    DataSource loaders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
+import axios from "axios";
+import { CurrentUserLoader } from "./CurrentUserLoader";
+import { DataSource } from "./DataSource";
+import { ProductInfo } from "./ProductInfo";
+import { ResourceLoader } from "./ResourceLoader";
+import {UserInfo} from "./UserInfo";
+import { UserLoader } from "./UserLoader";
+
+const getServerData = url=>async()=>(await axios.get(url)).data;
+
+const getLocalStorageData = key=>()=>{
+	return localStorage.getItem(key);
+}
+
+const Text = ({message})=><h1>{message}</h1>
+
 function App() {
 	return (
-		<h1>Your code goes here</h1>
+		<>
+		<DataSource getDataFunc={getLocalStorageData("message")} resourceName={"message"}>
+			<Text/>
+		</DataSource>
+		<DataSource getDataFunc={getServerData('/users/345')} resourceName={"user"}>
+			<UserInfo/>
+		</DataSource>
+		
+		<ResourceLoader resourceUrl="/users/345" resourceName="user">
+			<UserInfo/>
+		</ResourceLoader>
+
+		<ResourceLoader resourceUrl="/products/4567" resourceName="product">
+			<ProductInfo/>
+		</ResourceLoader>
+
+		</>
 	);
 }
 
---
+++ src/CurrentUserLoader.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+
+export const CurrentUserLoader = ({children})=>{
+
+    const [user, setUser] = useState(null);
+
+    useEffect(()=>{
+        (async ()=>{
+            const response = await axios.get('/current-user');
+            const currentUser = response.data;
+            setUser(currentUser);
+
+        })();
+    },[]);
+
+
+    return (
+        <>
+        {React.Children.map(children, child=>{
+            if (React.isValidElement(child)) {
+                return React.cloneElement(child, {user});
+            }
+            return child
+        })}
+        </>
+    )
+}
